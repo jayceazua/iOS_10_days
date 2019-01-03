def0a69055cc1e0d2672ba1c8a9b3df641ac8ba4
Jayce Azua <jayce.azua@gmail.com>
2019-01-03 02:08:58 -0500

    Initial Commit

---
+++ Swift Fundamentals/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift Fundamentals
+//
+//  Created by Jayce Azua on 1/3/19.
+//  Copyright © 2019 Jayce Azua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
